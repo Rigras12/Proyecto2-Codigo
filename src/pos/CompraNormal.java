1c8edf04f900ae72717055db27d83ae388147b4c
JackRakham <j.villatet@uniandes.edu.co>
2021-11-28 15:59:09 -0500

    df

--- src/pos/CompraNormal.java
+++ src/pos/CompraNormal.java
@@ -1,14 +1,20 @@
 package pos;
 
 import Inventario.Inventario;
+import Inventario.Producto;
 
 public class CompraNormal implements MetodoCompra {
 
 	
 	public int AplicarOferta(int numero, int codigo, int cantidad, Cliente cliente, Inventario inventario,
 			supermercado mercado) {
-		
-		return 0;
+		Producto producto = inventario.getProducto(codigo);
+		double precio = producto.getPrecioCantidad(cantidad);
+		producto.setbalance(producto.getBalance()+precio);
+		if (cliente!=null) {
+			cliente.agregarPuntos((int)precio/1000);
+		}
+		return (int)precio;
 	}
 
 }
